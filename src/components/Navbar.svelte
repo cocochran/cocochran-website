e354325ba8231a07b8450541e5f4219c2af9d4a9
Connor M. Cochran <connor.cochran@outlook.com>
2024-08-15 01:43:38 -0400

    navbar update

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -2,5 +2,14 @@
     import "../app.css";
 </script>
 <main>
-    <p>I need you to f right off</p>
+    <div class="border border-4 mx-[25%] my-3 py-5 rounded-2xl shadow-xl">
+        
+        <button class="ml-10">Home</button>
+        <h1 class="mx-auto text-indigo-950">HELLO</h1>
+        <div class="space-x-10 mr-10">
+        <button>Portfolio</button>
+        <button>About</button>
+        </div>
+
+    </div>
 </main>
