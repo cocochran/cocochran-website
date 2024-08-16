7511beb8ba5f99fae57f0efcd684e54c5201078e
Connor M. Cochran <connor.cochran@outlook.com>
2024-08-15 22:36:47 -0400

    changes to index & added icons folder

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -14,14 +14,7 @@
         
         <button class="ml-10">Home</button>
         <div class="mx-auto text-indigo-950" >
-            <Typewriter mode=loopOnce>
-                <h1>Hi!</h1>
-                <h1>My name is Connor Cochran.</h1>
-                <h1>I program.</h1>
-                <h1>I am currently a student at The Ohio State University.</h1>
-                <h1>I study Computer & Information Science.</h1>
-                <h1>Connor M. Cochran</h1>
-            </Typewriter>
+            
 
 
             
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,23 @@
 <script lang="ts">
   import "../app.css";
+    import Typewriter, { loop } from 'svelte-typewriter';
+
   
 </script>
 
 <head>
     <title>Connor M. Cochran</title>
 </head>
-
-
+<div class="border border-4 my-3 mx-10 py-5 rounded-2xl shadow-xl">
+  <Typewriter mode=loopOnce cursor={false}>
+                  <h1>Hi!</h1>
+                  <h1>My name is Connor Cochran.</h1>
+                  <h1>I program.</h1>
+                  <h1>I am currently a student at The Ohio State University.</h1>
+                  <h1>I study Computer & Information Science.</h1>
+                  <h1>Connor M. Cochran</h1>
+  </Typewriter>
+</div>
 <body class="">
     <p>Finally</p>
 </body>
